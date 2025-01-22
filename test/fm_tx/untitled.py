781246672e7c787bd796e942870acf317169bcbf
aruncs31s <aruncs31ss@gmail.com>
2025-01-22 13:20:15 +0530

    update Wed Jan 22 01:20:15 PM IST 2025

--- test/fm_tx/untitled.py
+++ test/fm_tx/untitled.py
@@ -12,10 +12,9 @@
 from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio import analog
-from gnuradio import audio
-from gnuradio import filter
-from gnuradio.filter import firdes
+from gnuradio import blocks
 from gnuradio import gr
+from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
@@ -23,13 +22,12 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import osmosdr
-import time
+from gnuradio import iio
 import threading
 
 
 
-class fm_radio_test(gr.top_block, Qt.QWidget):
+class untitled(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
@@ -52,7 +50,7 @@ class fm_radio_test(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "fm_radio_test")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "untitled")
 
         try:
             geometry = self.settings.value("geometry")
@@ -71,55 +69,32 @@ class fm_radio_test(gr.top_block, Qt.QWidget):
         # Blocks
         ##################################################
 
-        self.rtlsdr_source_0 = osmosdr.source(
-            args="numchan=" + str(1) + " " + ""
+        self.iio_pluto_sink_0 = iio.fmcomms2_sink_fc32('192.168.2.1' if '192.168.2.1' else iio.get_pluto_uri(), [True, True], 32768, False)
+        self.iio_pluto_sink_0.set_len_tag_key('')
+        self.iio_pluto_sink_0.set_bandwidth(20000000)
+        self.iio_pluto_sink_0.set_frequency(int(100e6))
+        self.iio_pluto_sink_0.set_samplerate(int(1e6))
+        self.iio_pluto_sink_0.set_attenuation(0, 10.0)
+        self.iio_pluto_sink_0.set_filter_params('Auto', '', 0, 0)
+        self.blocks_wavfile_source_0 = blocks.wavfile_source('/home/aruncs/Music/chammak.wav', True)
+        self.analog_wfm_tx_0 = analog.wfm_tx(
+        	audio_rate=int(48e3),
+        	quad_rate=int(192e3),
+        	tau=(75e-6),
+        	max_dev=75e3,
+        	fh=(-1.0),
         )
-        self.rtlsdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
-        self.rtlsdr_source_0.set_sample_rate(int(1e6))
-        self.rtlsdr_source_0.set_center_freq(int(100e3), 0)
-        self.rtlsdr_source_0.set_freq_corr(0, 0)
-        self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
-        self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
-        self.rtlsdr_source_0.set_gain_mode(False, 0)
-        self.rtlsdr_source_0.set_gain(20, 0)
-        self.rtlsdr_source_0.set_if_gain(20, 0)
-        self.rtlsdr_source_0.set_bb_gain(20, 0)
-        self.rtlsdr_source_0.set_antenna('', 0)
-        self.rtlsdr_source_0.set_bandwidth(0, 0)
-        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
-                interpolation=5,
-                decimation=20,
-                taps=[],
-                fractional_bw=0)
-        self.low_pass_filter_0 = filter.fir_filter_ccf(
-            1,
-            firdes.low_pass(
-                1,
-                1e6,
-                int(100e3),
-                int(50e3),
-                window.WIN_BLACKMAN,
-                6.76))
-        self.audio_sink_0 = audio.sink(48000, '', True)
-        self.analog_wfm_rcv_0 = analog.wfm_rcv(
-        	quad_rate=240e3,
-        	audio_decimation=5,
-        )
-        self.analog_agc_xx_0 = analog.agc_cc((1e-4), 1.0, 1.0, 65536)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_agc_xx_0, 0), (self.analog_wfm_rcv_0, 0))
-        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.analog_agc_xx_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.rtlsdr_source_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.analog_wfm_tx_0, 0), (self.iio_pluto_sink_0, 0))
+        self.connect((self.blocks_wavfile_source_0, 0), (self.analog_wfm_tx_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "fm_radio_test")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "untitled")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -135,7 +110,7 @@ class fm_radio_test(gr.top_block, Qt.QWidget):
 
 
 
-def main(top_block_cls=fm_radio_test, options=None):
+def main(top_block_cls=untitled, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
